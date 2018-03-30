750bae53426f969888811a3995071e6dce9784f6
Brent Bradbury <bbradbury@xevo.com>
2018-03-29 18:00:03 -0700

    Skel

---
+++ main.go
@@ -0,0 +1,12 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// vendor tool to copy external source code from GOPATH or remote location to the
+// local vendor folder. See README.md for usage.
+package main
+
+func main() {
+
+
+}
